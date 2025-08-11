0497c5989f39a5e9df276dcd283d239912ac4eb1
hwangdaesun <hds650588@gmail.com>
2025-08-11 22:07:02 +0900

    feat : init project

---
+++ src/main/java/com/example/fcmretryplayground/FcmRetryPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.example.fcmretryplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmRetryPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcmRetryPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fcmretryplayground/FcmRetryPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fcmretryplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcmRetryPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
