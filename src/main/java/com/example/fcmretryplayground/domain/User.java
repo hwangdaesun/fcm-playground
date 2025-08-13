0ceb26633e86735248890eac446fb3404651b040
hwangdaesun <hds650588@gmail.com>
2025-08-14 00:01:46 +0900

    feat : User Entity 생성

---
+++ src/main/java/com/example/fcmretryplayground/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.fcmretryplayground.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "USER")
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "EMAIL", length = 50, unique = true)
+    private String email;
+}
