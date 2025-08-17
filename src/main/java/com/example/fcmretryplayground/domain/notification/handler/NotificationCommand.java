9d9e94a0e62559b42c6f1ffab4e2064f8654a77a
hwangdaesun <hds650588@gmail.com>
2025-08-17 22:57:25 +0900

    refactor : 사용하지 않는 import문 제거

--- src/main/java/com/example/fcmretryplayground/domain/notification/handler/NotificationCommand.java
+++ src/main/java/com/example/fcmretryplayground/domain/notification/handler/NotificationCommand.java
@@ -1,8 +1,5 @@
 package com.example.fcmretryplayground.domain.notification.handler;
 
-import com.example.fcmretryplayground.domain.notification.handler.NotificationType;
-import com.example.fcmretryplayground.domain.notification.handler.Recipient;
-import com.example.fcmretryplayground.domain.notification.handler.Sender;
 import java.util.List;
 
 public record NotificationCommand(
