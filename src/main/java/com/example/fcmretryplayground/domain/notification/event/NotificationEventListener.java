8ce3e45c0bce3147942e995d2b45aecf93f0c18b
hwangdaesun <hds650588@gmail.com>
2025-09-13 10:39:33 +0900

    fix : NotificationEventListener에서 NotificationService 필드에 공백 추가

--- src/main/java/com/example/fcmretryplayground/domain/notification/event/NotificationEventListener.java
+++ src/main/java/com/example/fcmretryplayground/domain/notification/event/NotificationEventListener.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 public class NotificationEventListener {
 
     private final NotificationService notificationService;
+
     @Async
     @EventListener
     public void execute(NotificationEvent event) {
