860189d8386e612c540f96a968d984d9767e3c75
hwangdaesun <hds650588@gmail.com>
2025-09-13 10:38:41 +0900

    feat : NotificationLogService 클래스 생성

---
+++ src/main/java/com/example/fcmretryplayground/domain/notification/NotificationLogService.java
@@ -0,0 +1,36 @@
+package com.example.fcmretryplayground.domain.notification;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.MessagingErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationLogService {
+
+    private final NotificationLogRepository notificationLogRepository;
+    private final ObjectMapper mapper;
+
+    @Transactional
+    public void recordNotificationLog(DeviceFcmToken deviceFcmToken, Message message, MessagingErrorCode code) {
+        try {
+            deviceFcmToken.markInvalid();
+            NotificationLog failLog = NotificationLog.record(
+                    deviceFcmToken.getId(), serializeMessage(message), code);
+            notificationLogRepository.save(failLog);
+            log.info("Fail Notification Log 기록 성공: {}", failLog);
+        } catch (Exception e) {
+            log.error("Fail Notification Log 기록 실패 : {}", e.getMessage());
+        }
+    }
+
+    private String serializeMessage(Message message) throws JsonProcessingException {
+        return mapper.writeValueAsString(message);
+    }
+}
